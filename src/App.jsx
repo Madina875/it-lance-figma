370abc7094929f287ee26a4d440652662227e46e
madina875 <evaevasmq@gmail.com>
2025-08-27 01:48:20 +0500

    hmw

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/header/Header.jsx";
-import Footer from "./components/Footer/Footer.jsx";
-import Hero from "./components/Hero/Hero.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Hero from "./components/hero/Hero.jsx";
 import More from "./components/more/More.jsx";
 import Skills from "./components/skills/Skills.sjx";
 
