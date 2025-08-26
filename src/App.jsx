7573ea72aab9359aead3aea9a02038fc8b481fbc
madina875 <evaevasmq@gmail.com>
2025-08-27 01:49:14 +0500

    hmw

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/header/Header.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Hero from "./components/hero/Hero.jsx";
 import More from "./components/more/More.jsx";
-import Skills from "./components/skills/Skills.sjx";
+import Skills from "./components/skills/Skills.jsx";
 
 const App = () => {
   return (
